f016a7d52cd6a36cfd16238a4318c314cf36ece3
avena <fernando.avena@gmail.com>
2025-09-01 10:25:50 -0300

    exercicio 03

--- code.R
+++ code.R
@@ -4,6 +4,16 @@ library(rafalib)
 install.packages('swirl')
 library(swirl)
 
-# Exercicio 02
+# Exercício 02
 vetorValores <- c(2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23)
 media <- mean(vetorValores)
+
+# Exercício 03
+soma <- 0
+for (i in 1:25) {
+  soma <- soma + i^2
+}
+soma
+
+# Formula
+#sum((1:25)^2)
